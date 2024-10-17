c7a8563cf31706bcf051aa1a026479ffe9448281
Th3Mayar <josehenriquez.02.26@gmail.com>
2024-10-16 20:04:44 -0400

    chore: modify params and add structure return messages

--- lib/src/utils/lists_maps.dart
+++ lib/src/utils/lists_maps.dart
@@ -1,3 +1,5 @@
+import '../widgets/divider.dart' show lineBreak;
+
 // Print List and Map values
 void printList(List<dynamic> list) {
   for (var item in list) {
@@ -7,22 +9,38 @@ void printList(List<dynamic> list) {
 
 // Print Map values
 void printMap(Map<String, dynamic> map) {
+  print('Map contains the following key-value pairs:');
   map.forEach((key, value) {
-    print('$key: $value');
+    print('Key: $key, Value: $value');
   });
 }
 
 // Function to handle lists
 void handleList(List<int> list) {
-  print(list);
-  print(list[0]);
-  print(list.length);
+  print('All list elements: $list');
+  print('First element: ${list[0]}');
+  print('Total number of elements: ${list.length}');
 }
 
 // Function to handle maps
-void handleMap(Map<String, dynamic> persona) {
-  print(persona);
-  print(persona['name']);
-  print(persona['age']);
-  print(persona.length);
+void handleMap(Map<String, dynamic> person) {
+  // Print each key-value pair
+  person.forEach((key, value) {
+    print('$key: $value');
+  });
+
+  // Access specific parameters if necessary
+  print("\nSpecific details:");
+  print('Name: ${person['name']}');
+  print('Last Name: ${person['lastName']}');
+  print('Age: ${person['age']}');
+
+  // If any additional parameters exist, print them
+  person.forEach((key, value) {
+    if (!['name', 'lastName', 'age'].contains(key)) {
+      print('Additional parameter - $key: $value');
+    }
+  });
+
+  lineBreak();
 }
